e1f675b06ac3ba6114bc2a5c762af59ccfcee702
Maciej Witkowski <maciekwitkowski322@gmail.com>
2020-12-30 16:51:36 +0100

    [B] Database related bugs fixed - connection bug fixed - bad database structure fixed

--- DataBaseProvider.py
+++ DataBaseProvider.py
@@ -2,36 +2,38 @@ import sqlite3
 from sqlite3 import Error
 
 class DataBaseProvider:
-    def __init__(self, db_file):
+    def __init__(self, db_file="re_book.db"):
         self.conn = None
+        self.db_name = db_file
+        self.__init_database()
 
     def __create_table(self, query):
         try:
+            self.__open_connection()
             cur = self.conn.cursor()
             cur.execute(query)
             print('[DB] Table created successfully')
-        except Error as e:
-            print('[DB] Error while creating table')
-        finally:
             self.__close_conection()
-            
+        except Error as e:
+            print(e)
+
     def __close_conection(self):
         self.conn.commit()
         self.conn.close()
 
-    def __open_connection(self, db_file="ReBook"):
+    def __open_connection(self):
         try:
-            self.conn = sqlite3.connect(f'./{db_file}')
+            self.conn = sqlite3.connect(f'./{self.db_name}')
         except Error as e:
             print(e)
 
     def __init_database(self):
-        # TODO: Create all tables if not existing
         sql_create_books_table = """ CREATE TABLE IF NOT EXISTS books (
                                         _IdB integer PRIMARY KEY,
                                         Name text NOT NULL,
                                         Author text NOT NULL,
-                                        Year text NOT NULL
+                                        Year text NOT NULL,
+                                        UNIQUE(Name, Author, Year)
                                     );"""                  
         sql_create_logs_table = """CREATE TABLE IF NOT EXISTS logs (
                                         _IdL integer PRIMARY KEY,
@@ -44,26 +46,30 @@ class DataBaseProvider:
                                         _IdU integer PRIMARY KEY,
                                         Login text NOT NULL,
                                         Password text NOT NULL,
-                                        Name text NOT NULL
+                                        Name text NOT NULL,
+                                        UNIQUE(Login)
                                     );"""
         sql_create_opinions_table = """CREATE TABLE IF NOT EXISTS opinions (
                                         _IdO integer PRIMARY KEY,
                                         IdB integer NOT NULL,
                                         IdU integer NOT NULL,
-                                        Payload text NOT NULL
+                                        Payload text NOT NULL,
                                         Ranking integer NOT NULL,
+                                        UNIQUE(IdB, IdU)
+                                        FOREIGN KEY (IdU) REFERENCES users (_IdU)
+                                        FOREIGN KEY (IdB) REFERENCES books (_IdB)
+
                                     );"""
 
-        self.__open_connection()
         self.__create_table(sql_create_users_table)
         self.__create_table(sql_create_books_table)
         self.__create_table(sql_create_logs_table)
         self.__create_table(sql_create_opinions_table)
-        self.__close_conection()
 
     def add_book(self, name, author, year):
         sql = f' INSERT INTO books(Name,Author,Year) VALUES("{name}","{author}","{year}");'
         try:
+            self.__open_connection()
             cur = self.conn.cursor()
             cur.execute(sql)
         except Error as e:
